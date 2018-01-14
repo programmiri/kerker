6c034ac645e294fdcf4a5e64cd7ff6dfec6861c7
programmiri <programmiri@gmail.com>
2018-01-14 12:47:38 +0100

    Add PropTypes and use it in Alert component

--- src/components/Alert.test.js
+++ src/components/Alert.test.js
@@ -7,17 +7,17 @@ it('renders without crashing', () => {
 });
 
 describe('renders a alert with a custom message und custom color schema', () => {
-  const wrapper = shallow(<Alert text="test text" context="test"/>);
+  const wrapper = shallow(<Alert text="This is a warning" context="warning"/>);
 
   it('renders a div with the class alert', () => {
     expect(wrapper.hasClass('alert')).toEqual(true);
   });
 
   it('displays a custom text', () => {
-    expect(wrapper.text()).toEqual('test text');
+    expect(wrapper.text()).toEqual('This is a warning');
   });
 
   it('adds a custom class for context', () => {
-    expect(wrapper.hasClass('alert-test')).toEqual(true);
+    expect(wrapper.hasClass('alert-warning')).toEqual(true);
   });
 })
