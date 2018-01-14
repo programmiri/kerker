fa44265d6d33624c79739f6b09b01f20a244ecc1
programmiri <programmiri@gmail.com>
2018-01-14 12:27:07 +0100

    Style custom, using bootstrap classes and placeholer logo.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './../images/logo.svg';
+import logo from './../images/logo.png';
 import Alert from './Alert';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App container-fluid">
+        <header className="App-header d-block">
           <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-title">Welcome to Kerker</div>
+          <p className="h4">Welcome to Kerker</p>
         </header>
         <Alert text="This is the text" context="primary"/>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
