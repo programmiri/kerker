d7203eb7d467803a7fe8d15182e53a35f6e3216b
Christian Bäuerlein <fabrik42@gmail.com>
2018-01-16 14:28:48 +0100

    Add usage example of encryption adapters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import * as trezor from './lib/encryption_adapters/trezor';
+import * as dummy from './lib/encryption_adapters/dummy';
+
 import registerServiceWorker from './registerServiceWorker';
+
+const encryptionAdapters = {
+  dummy: dummy,
+  trezor: trezor
+};
+
+const encryption = encryptionAdapters.trezor;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+// TODO: Just for manual tryouts, remove when in UI
+window.encryption = encryption;
