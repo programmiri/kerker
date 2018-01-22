f42af4732a1f58a1e2f958a17f9bd9472fab40bf
programmiri <programmiri@gmail.com>
2018-01-22 16:22:03 +0100

    Add tests for List and ListElement

--- src/components/List.test.js
+++ src/components/List.test.js
@@ -5,3 +5,12 @@ import List from './List';
 it('renders without crashing', () => {
   shallow(<List />);
 });
+
+describe('renders a list of notes', () => {
+  const wrapper = shallow(<List />);
+
+  it('renders an ul with the class list-group', () => {
+    expect(wrapper.find('ul').exists()).toEqual(true);
+    expect(wrapper.hasClass('list-group')).toEqual(true);
+  });
+})
--- src/components/ListItem.test.js
+++ src/components/ListItem.test.js
@@ -5,3 +5,18 @@ import ListItem from './ListItem';
 it('renders without crashing', () => {
   shallow(<ListItem />);
 });
+
+describe('renders a list of notes', () => {
+  const wrapper = shallow(<ListItem name="This is the name of the note" />);
+
+  it('renders an li element with the class list-group-item', () => {
+    expect(wrapper.hasClass('list-group-item')).toEqual(true);
+  });
+
+  it('displays a custom text as name', () => {
+    expect(wrapper.text()).toEqual('This is the name of the note');
+  });
+
+
+
+})
