01a1a3eaa9e6feb7cc5f74f942f7f4977f3d44e0
Christian Bäuerlein <fabrik42@gmail.com>
2018-01-28 19:21:12 +0100

    Add example data to app container

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import App from './App';
 
 class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: '109156be-c4fb-41ea-b1b4-efe1671c5836',
+          title: 'Hello',
+          body: 'This is my body',
+          bodyPersistedVersion: 'This is my body',
+          bodyEncrypted: 'SECRET',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: '209156be-c4fb-41ea-b1b4-efe1671c5836',
+          title: 'Another note',
+          body: 'Another body',
+          bodyPersistedVersion: 'Another body',
+          bodyEncrypted: 'SECRET2',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <App />
+      <App notes={this.state.notes} />
     );
   }
 }
